ec9ca6b559c34f5d628083b8282e3f88584acea2
eikmeier <martin@educlick.com.br>
2026-08-19 08:54:44 -0300

    consumo lms v1

--- front_end/app/components/programacao_atividades/UploadArquivo.vue
+++ front_end/app/components/programacao_atividades/UploadArquivo.vue
@@ -115,9 +115,11 @@ function triggerInput() { fileInput.value?.click(); }
 
 async function handleFile(event: Event) {
     const input = event.target as HTMLInputElement;
-    if (!input.files || input.files.length === 0) return;
+    const file = input.files?.[0];
+    if (!file) {
+      return;
+    }
 
-    const file = input.files[0];
     fileName.value = file.name;
     uploading.value = true;
 
